955735f8a8041e9de98152c2f51ccce4797db782
Santiago Gimenez <sagimenez9@gmail.com>
2022-08-23 18:32:02 -0300

    Initial

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let opSys = require('os');
+let app = express();
+
+app.get('/', function(req, res) {
+    let request = {
+        headers: req.headers,
+        body: req.body,
+        method: req.method
+    };
+    console.log(request);
+    res.json({message: 'Ok'})
+})
+
+app.listen(3000);
+console.log(`Listening on ${opSys.hostname()}:3000`);
