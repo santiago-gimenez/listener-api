6db34664ba20b181bd5bca14d3eb68ef80058974
Santiago Gimenez <42209978+santiago-gimenez@users.noreply.github.com>
2023-03-18 03:57:12 +0000

    add gms-wo-ops

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ let express = require('express');
 let opSys = require('os');
 let app = express();
 
-app.get('/', function(req, res) {
-    let request = {
-        headers: req.headers,
-        body: req.body,
-        method: req.method
-    };
-    console.log(request);
-    res.json({message: 'Ok'})
-})
+app.use(express.static('public'))
+
+
+// app.get('/', function(req, res) {
+//     let request = {
+//         headers: req.headers,
+//         body: req.body,
+//         method: req.method
+//     };
+//     console.log(request);
+//     res.json({message: 'Ok'})
+// })
 
 app.listen(3000);
 console.log(`Listening on ${opSys.hostname()}:3000`);
